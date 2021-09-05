cdfc4d989ae0729fcd02c2b36303f1f1785a9c7e
liam34 <indemandcoding@gmail.com>
2021-09-05 15:41:02 -0600

    update

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
 const router = express.Router();
 
 //Login page
-router.get('/login', (req, res) => res.send('Login'));
+router.get('/login', (req, res) => res.render('login'));
 
 //register page
-router.get('/register', (req, res) => res.send('Register'));
+router.get('/register', (req, res) => res.render('register'));
 
+//register handle
+router.post('register', (req, res) => {
+    console.log(req.body)
+    res.send('hello')
+
+})
 
 module.exports = router;
